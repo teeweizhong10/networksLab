5d71db9c94c89d723edfdbb1677c858f97509963
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-03-02 14:25:14 -0600

    Add experiment files

---
+++ experimentalCode/testClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Wei Zhong Tee on 3/2/22.
+//
+
+#include "testClient.h"
---
+++ experimentalCode/testServer.h
@@ -0,0 +1,14 @@
+//
+// Created by Wei Zhong Tee on 3/2/22.
+//
+
+#ifndef NETWORKSLAB_TESTSERVER_H
+#define NETWORKSLAB_TESTSERVER_H
+
+
+class testServer {
+
+};
+
+
+#endif //NETWORKSLAB_TESTSERVER_H
