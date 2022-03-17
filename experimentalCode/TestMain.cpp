1fe5faf6a1192a959f007a8cf16879d13b544fe3
Kyle Sargeant <Sargeakb9348@uwec.edu>
2022-03-17 11:10:52 -0500

    Test to run read file and bit set should print bits

--- experimentalCode/TestMain.cpp
+++ experimentalCode/TestMain.cpp
@@ -16,7 +16,7 @@ int main()
 {
     vector<char> bytes;
     char byte = 0;
-    string bits='';
+    string bits;
     //just try read into char vector
     //then convert to binary
 
@@ -29,7 +29,7 @@ int main()
     }else{
         while(input_file.get(byte)){
             //bytes.push_back(byte);
-            bits=bitset<8>(byte).to_string());
+            bits=bitset<8>(byte).to_string();
             cout << bits;
         }
 
