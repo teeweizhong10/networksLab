cb1b876cd3782835269ef1b832aa19822724a299
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-03-16 23:41:01 -0500

    Update makefiles for boost

--- makeSender.mk
+++ makeSender.mk
@@ -3,7 +3,7 @@ CFLAGS = -Wall -g #debug flag
 #CFLAGS = -Wall
 
 sender: sender.o
-	$(CC) $(CFLAGS) -o sender sender.o
+	$(CC) $(CFLAGS) -o sender sender.o -lboost_system -lpthread
 
 sender.o: sender.cpp packet.h
 	$(CC) $(CFLAGS) -c sender.cpp
