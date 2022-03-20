617efa68c1fd01b1ebb91fac4b3682c2c13d6fa0
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-03-20 13:54:44 -0500

    Finish making packet class

--- packet.cpp
+++ packet.cpp
@@ -2,4 +2,33 @@
 // Created by Wei Zhong Tee on 3/6/22.
 //
 
+#include <iostream>
+#include <string>
 #include "packet.h"
+
+packet::packet() {
+    packetNum = -1;
+    seqNum = -1;
+    bitContent = "";
+    checksumValue = -1;
+    packetMessage = "";
+}
+
+packet::packet(int packetNum, int seqNum, string bitContent, int checksumValue) {
+    setPacketNum(packetNum);
+    setSeqNum(seqNum);
+    setBitContent(bitContent);
+    setChecksumValue(checksumValue);
+}
+
+void packet::setPacketMessage() {
+    string message =std::to_string(packetNum);
+    message += "|";
+    message += std::to_string(seqNum);
+    message += "|";
+    message += bitContent;
+    message += "|";
+    message += std::to_string(checksumValue);
+    packetMessage = message;
+}
+
--- packet.h
+++ packet.h
@@ -1,14 +1,44 @@
 //
 // Created by Wei Zhong Tee on 3/6/22.
 //
-
+#include <string>
+#include <cstring>
 #ifndef NETWORKSLAB_PACKET_H
 #define NETWORKSLAB_PACKET_H
 
+using namespace std;
 
 class packet {
+private:
+    int packetNum;
+    int seqNum;
+    string bitContent;
+    int checksumValue; //TODO: check checksum value with Lauren
+    string packetMessage;
 
+public:
+    packet();
+    packet(int packetNum, int seqNum, string bitContent, int checksumValue);
+
+    void setPacketNum(int input) {packetNum = input;};
+    int getPacketNum() {return packetNum;};
+
+    void setSeqNum(int input) {seqNum = input;};
+    int getSeqNum() {return seqNum;};
+
+    void setBitContent (string bitInput) {bitContent = bitInput;};
+    string getBitContent() {return bitContent;};
+
+    void setChecksumValue(int input) {checksumValue = input;};
+    int getChecksumValue() {return checksumValue;};
+
+    void setPacketMessage();
+    string getPacketMessage() {return packetMessage;};
 };
 
+/* Plan for packet structure that will be sent
+ * Packet message: "packetNum|seqNum|bitContent|checksumVal"
+ * "2|23|0101010101000|010100"
+ * */
 
 #endif //NETWORKSLAB_PACKET_H
