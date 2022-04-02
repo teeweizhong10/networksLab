dfcdb0fbbc57f27723f690fe2ceebce6d06d874b
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-04-02 13:00:54 -0500

    set buffer size

--- sender.cpp
+++ sender.cpp
@@ -573,8 +573,8 @@ string getData(tcp::socket& socket){
 }
 
 void sendData(tcp::socket& socket, const string& msg){
-//    boost::asio::socket_base::send_buffer_size option(sizeOfPacket + 40);
-//    socket.set_option(option);
+    boost::asio::socket_base::send_buffer_size option(sizeOfPacket + 40);
+    socket.set_option(option);
     const string& temp = msg + "=|||=";
     cout << "Sending data: " << temp << endl;
     boost::asio::write(socket, buffer(temp));
