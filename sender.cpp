ddf28006d2222d5d7c684c03e618098eddb243b1
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-04-02 13:04:17 -0500

    print buffer size

--- sender.cpp
+++ sender.cpp
@@ -575,8 +575,6 @@ string getData(tcp::socket& socket){
 void sendData(tcp::socket& socket, const string& msg){
     boost::asio::socket_base::send_buffer_size option(sizeOfPacket + 40);
     socket.set_option(option);
-    boost::asio::socket_base::send_buffer_size option;
-    socket.get_option(option);
     int size = option.value();
     const string& temp = msg + "=|||=";
     cout << "Sending data: " << temp << endl;
