60987275cb986294196da888164788271f85d76f
Wei Zhong Tee <weizhong.tee@jamf.com>
2022-04-07 17:00:50 -0500

    SR changes under fillArray

--- sender.cpp
+++ sender.cpp
@@ -911,6 +911,7 @@ void SR(tcp::socket& socket, vector<char> bytes){
         sendPacket(socket, packetsAr[p]);
         cout << "Packet " << to_string(packetsAr[p].getPacketNum()) << " sent" << endl;
         acksRecv.push_back(getData(socket));
+        cout << "Received ack test" << endl;
     }
 
 
